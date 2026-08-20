da0d49feee407b355979b4e9b47680cbc9aa20b9
Kingjaw <mondattam@gmail.com>
2026-08-20 01:41:44 -0400

    404 page

--- games/gamePage.js
+++ games/gamePage.js
@@ -1,12 +1,16 @@
-function playGame(){
+function playGame(game){
   document.getElementById("pButton").remove();
   document.getElementById("gOverlay").remove();
 
   const gameWindow=document.createElement("iframe");
   
-  gameWindow.src="GameBuild/index.html";
+  var test="Donalia3"
+  gameWindow.src=`https://assets.kingjaw.com/Games/${test}/GameBuild/index.html`;
   gameWindow.width="960"; gameWindow.height="800";
   gameWindow.scrolling="none";
   gameWindow.frameBorder=0;
+  gameWindow.setAttribute("allow","fullscreen");
+  gameWindow.setAttribute("allowfullscreen","true");
+
   document.getElementById("centered").appendChild(gameWindow); 
 }
